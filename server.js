f7d217fa65a4a6d9478d129df32db598c5db03ee
Erik <erikpersson0884@gmail.com>
2026-08-17 17:23:25 +0200

    added manifest to login page to

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session({
 // --- Auth middleware: blocks everything below unless logged in ---
 function requireAuth(req, res, next) {
   if (req.session && req.session.loggedIn) return next();
-  if (req.path === "/login" || req.path === "/login.html" || req.path==="/styles.css") return next();
+  if (req.path === "/login" || req.path === "/login.html" || req.path==="/styles.css" || req.path==="/favicon.png") return next();
   return res.redirect("/login.html");
 }
 
